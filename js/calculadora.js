47452b6bb082c67b47745a88ade5ca34697b2539
Jhesllux <jhonestebansar@gmail.com>
2025-07-30 10:20:12 -0500

    Update calculadora.js

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,19 +1,19 @@
-        function calcularEdadPerruna() {
-            let edadHumana = parseInt(document.getElementById("edad").value);
-            let sexo = document.getElementById("sexo").value;
-            let nombre = document.getElementById("nombre").value;
-            let edadPerruna = 0;
-            let i = 0;
+function calcularEdadPerruna() {
+    let edadHumana = parseInt(document.getElementById("edad").value);
+    let sexo = document.getElementById("sexo").value;
+    let nombre = document.getElementById("nombre").value;
+    let edadPerruna = 0;
+    let i = 0;
 
-            while (i < edadHumana) {
-                if (sexo === "mujer") {
-                    edadPerruna += 5;
-                } else if (sexo === "hombre") {
-                    edadPerruna += 7;
-                }
-                i++;
-            }
-
-            document.getElementById("resultado").innerText =
-                nombre + ", tu edad perruna es: " + edadPerruna + " años.";
+    while (i < edadHumana) {
+        if (sexo === "mujer") {
+            edadPerruna += 5;
+        } else if (sexo === "hombre") {
+            edadPerruna += 7;
         }
+        i++;
+    }
+
+    document.getElementById("resultado").innerText =
+        nombre + ", tu edad perruna es: " + edadPerruna + " años.";
+}
